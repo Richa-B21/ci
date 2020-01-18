78cc095de19c55cd5bcb37063f5785f0de5d71e8
Richa <837038@cognizant.com>
2020-01-18 16:00:17 +0530

    Signed-off-by: Richa <Richa-B21@github.com>

--- CI.java
+++ CI.java
@@ -1,5 +1,5 @@
 class CI{  
     public static void main(String args[]){  
-     System.out.println("new 123 jenkins project ");  
+     System.out.println("new 12345 jenkins project ");  
     }  
 }  
